c81d0f4e98ccf4bfe255509f1119d3a19a7557b5
Michael Copanos <michaelcopanos@Michaels-MBP.attlocal.net>
2019-01-31 19:46:05 -0600

    home page inital set up

--- src/Layout.js
+++ src/Layout.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Header from './components/Header'
+import Home from './components/Home'
 
 class Layout extends Component {
   constructor(){
     super();
     this.state = {
-      navLinks: ['Home', 'About Me', 'Projects', 'Accomplishments', 'Contact Me']
+      navLinks: ['Home', 'About Me', 'Projects', 'Accomplishments', 'Contact Me'],
+      msg: {
+        home: 'This is the Home Page',
+        about: 'This is the About Me Page',
+        projects: 'This is the Project Page',
+        accomplishments: 'This is the Accomplishments Page',
+        contact: 'This is the Contact Me Page'
+      }
     }
   }
   render() {
+    const { navLinks, msg} = this.state;
     return (
-        <Header links={this.state.navLinks}/>
+        <div>
+          <Header links={navLinks}/>
+          <Home message={msg}/>
+        </div>
+
     );
   }
 }
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React  from 'react';
+
+const Home = props => {
+    return(
+        <main>
+            <h1>{props.message.home}</h1>
+        </main>
+
+    )
+};
+
+export default Home;
