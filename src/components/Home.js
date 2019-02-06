aa3b254107f64e032661c5309730492da482e6af
Michael Copanos <michaelcopanos@Michaels-MBP.attlocal.net>
2019-02-05 18:49:15 -0600

    home page background

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,11 @@ import Background from '../assests/home-page.jpeg';
 const Home = () => {
     return(
         <main>
+            {/*<BackgroungImage/>*/}
             <img src={Background} alt={Background}/>
+            <div className="home-header">
+                <h1>Hi, I'm Mike Copanos!</h1>
+            </div>
         </main>
 
     )
